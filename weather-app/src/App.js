36c8bc247af95d119a57ee46092301a19dea5696
Egor Grishanov <egor4ik_grish@mail.ru>
2022-06-14 13:04:10 +0400

    step 3

--- weather-app/src/App.js
+++ weather-app/src/App.js
@@ -19,32 +19,57 @@ class App extends React.Component {
         country: undefined,
     }
 
-    getWeather = async (e) => {
-        e.preventDefault();
-        const citys = e.target.elements.city.value;
+    getWeather = async (event) => {
+        event.preventDefault();
+        let city = event.target.elements.city.value;
         const api_url = await 
-        fetch(`https://api.openweathermap.org/data/2.5/weather?q=${citys}&appid=${API}&units=metric`);
+        fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${API}&units=metric`);
         const dataWeather = await api_url.json();
         console.log(dataWeather);
         
+        if(city) {
+
+        function timeConverter(UNIX_timestamp){
+            let a = new Date(UNIX_timestamp * 1000);
+            let hour = a.getHours();
+            let min = "0" + a.getMinutes();
+            let sec = "0" + a.getSeconds();
+            let time = hour + ':' + min.substr(-2) + ':' + sec.substr(-2) ;
+            return time;
+        }
+            let sunset = dataWeather.sys.sunset;
+            let sunrise = dataWeather.sys.sunrise;
+            let sunset_date = timeConverter(sunset);
+            let sunrise_date = timeConverter(sunrise);
+
+            let pressure = dataWeather.main.pressure;
+            let pressureInMmHg = Math.floor(pressure * 0.75006);
+
+            let temp = dataWeather.main.temp;
+            let newTemp = Math.floor(temp);
+
+            let feels_like = dataWeather.main.feels_like;
+            let newFeels_like = Math.floor(feels_like);
+
             this.setState({
-                temp: dataWeather.main.temp,
+                temp: newTemp,
                 humidity: dataWeather.main.humidity,
-                sunset: dataWeather.sys.sunset,
-                sunrise: dataWeather.sys.sunrise,
-                pressure: dataWeather.main.pressure,
-                feels_like: dataWeather.main.feels_like,
+                sunset: sunset_date,
+                sunrise: sunrise_date,
+                pressure: pressureInMmHg,
+                feels_like: newFeels_like,
                 wind: dataWeather.wind.speed,
                 country: dataWeather.sys.country,
                 city: dataWeather.name,
             });
+        }
     }
 
     render() {
         return (
             <div>
                 <Info />
-                <Form  weatherForForm={this.getWeather}/>
+                <Form  weatherForForm={this.getWeather} />
                 <Weather
                 temp={this.state.temp}
                 humidity={this.state.humidity}
